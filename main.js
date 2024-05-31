0da0d97e94888c99ca7f30315c5bfd6428e03163
kailash <kailash.doodleblue@gmail.com>
2024-05-31 12:26:07 +0530

    added jwt

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ let mysql = require('mysql')
 let express = require('express')
 let bcrypt = require('bcryptjs')
 let bodyparser = require('body-parser')
+const jwt = require('jsonwebtoken');
 let con=require('./connecion')
 
 let app = express()
@@ -16,6 +17,7 @@ app.post ('/signup',async (req,res)=>{
         res.json({message : `New user created for ${username}`})
     })
 })
+var hhh
 app.post('/login',(req,res)=>{
     let {username,password} = req.body
     con.query(`select * from userdetails where userName="${username}"`,(err,data)=>{
@@ -30,8 +32,10 @@ app.post('/login',(req,res)=>{
                 // return res.json({message:`${username} login successful`})
                 return res.status(401).json({ error: 'Invalid password' });
             }
-            
-            res.json({message:`${username} login successful`}) 
+            hhh=jwt.sign({ username: user.userName }, 'hello',{ expiresIn: '1h' }) 
+            res.json({hhh})
+            console.log(hhh)
+            // res.json({message:`${username} login successful`}) 
             // return res.status(401).json({ error: 'Invalid password' });
             
         })   
@@ -41,4 +45,23 @@ app.post('/login',(req,res)=>{
     })
     // res.json({message:`${username} login successful`})
 })
+
+app.get('/me', async (req, res) => {
+    try {
+        // const token = req.headers.authorization.split(' ')[1];
+        const token = hhh;
+        if (!token) {
+            return res.status(401).json({ error: 'Token is required' });
+        }
+        jwt.verify(token, 'hello', (err, decoded) => {
+            if (err) {
+                return res.status(401).json({ error: 'Invalid token' });
+            }
+            // Here you can fetch user details from the database using the decoded user ID or username
+            res.json({ user: decoded });
+        });
+    } catch (error) {
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
 app.listen(8000)
